4041126fa1e268f5a52007a45b8c4667550b3340
Kevin Morales <kevinchomorave@gmail.com>
2025-08-28 12:27:55 -0500

    Todos los detalles

--- demo/src/main/java/com/example/demo/DTO/DetallesCompras/DetalleComprasDTO.java
+++ demo/src/main/java/com/example/demo/DTO/DetallesCompras/DetalleComprasDTO.java
@@ -6,7 +6,7 @@ public class DetalleComprasDTO {
     @JsonProperty("Fecha_Entrada")
     private  String Fecha_Entrada;
     @JsonProperty("Cantidad")
-    private  String Cantidad;
+    private  int Cantidad;
     @JsonProperty("ID_Proveedor")
     private  String ID_Proveedor;
     @JsonProperty("ID_Entrada")
@@ -23,11 +23,11 @@ public class DetalleComprasDTO {
         this.Fecha_Entrada = fecha_Entrada;
     }
 
-    public String getCantidad() {
+    public int getCantidad() {
         return this.Cantidad;
     }
 
-    public void setCantidad(String cantidad) {
+    public void setCantidad(int cantidad) {
         this.Cantidad = cantidad;
     }
 
