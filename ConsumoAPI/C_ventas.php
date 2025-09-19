f895bb1ccf2cc1614a18007a698f278401aa529f
Kevin Morales <kevinchomorave@gmail.com>
2025-09-18 19:35:00 -0500

    C_ventas.php, y la separacion de registro HTML/PHP

--- ConsumoAPI/C_ventas.php
+++ ConsumoAPI/C_ventas.php
@@ -1,7 +1,8 @@
 <?php
-$url = "http://localhost:8080/Ventas";
+$urlGet = "http://localhost:8080/Ventas";
+$urlPost = "http://localhost:8080/VentaRegistro";
 
-$consumo = file_get_contents($url);
+$consumo = file_get_contents($urlGet);
 if ($consumo === false) {
     die("Error al consumir el servicio.");
 }
@@ -28,5 +29,47 @@ foreach ($ventas as $linea) {
     }
 }
 
+//POST
+$respuesta = readline("¿Desea registrar una nueva venta? (s/n): ");
+
+if($respuesta === 's'){
+    $ID_Venta = readline("Ingrese el ID de la venta: ");
+    $Documento_Cliente = readline("Ingrese el documento del cliente: ");
+    $Documento_Empleado = readline("Ingrese el documento del empleado: ");
+
+$data = array(
+        "ID_Venta" => $ID_Venta,
+        "Documento_Cliente" => $Documento_Cliente,
+        "Documento_Empleado" => $Documento_Empleado
+    );
+    $data_json = json_encode($data);
+
+    $proceso = curl_init($urlPost);
+
+    curl_setopt($proceso, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($proceso, CURLOPT_POSTFIELDS, $data_json);
+    curl_setopt($proceso, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($proceso, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($data_json)
+    ));
+
+    $respuestapeticion = curl_exec($proceso);
+
+    $http_code = curl_getinfo($proceso, CURLINFO_HTTP_CODE);
+
+    if (curl_errno ($proceso)) {
+        die("Error en la petición POST" . curl_error($proceso) ."\n");
+    }
+
+    curl_close($proceso);
+
+
+    if ($http_code === 200){
+        echo "Venta registrada exitosamente.\n";
+    } else {
+        echo "Error al registrar la venta. Código HTTP: $http_code\n";
+    }
+}
 
 // pa que corra C:\xampp\php\php.exe C_ventas.php
